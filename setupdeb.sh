dae0878876acba2e9ad452f628665a5e136d9d87
Michael McMahon <michael.steven.mcmahon@gmail.com>
2018-02-02 20:28:39 -0500

    Create setupdeb.sh

---
+++ setupdeb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# setupdeb.sh v1.0
+
+# Michael McMahon
+
+# setup.sh installs the necessary pacakges to run builddatapack.sh
+# on Debian based GNU/Linux distributions.
+
+# To run, open a terminal and enter:
+#   sudo sh builddatapack.sh
+
+# Check for root.
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root"
+  exit 1
+fi
+
+apt update
+apt install -y python3-pip
+apt install -y python3-tk
+pip3 install pandas
+pip3 install matplotlib
+apt install -y gpicview
+apt install -y zip
