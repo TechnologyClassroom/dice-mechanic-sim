a45d26db8e5aa3c3476c76156cfe3c83dba649ef
Maria Ilie <maria.alex.ilie@gmail.com>
2020-10-14 00:00:29 -0700

    added license

--- data/plotcsv.py
+++ data/plotcsv.py
@@ -3,7 +3,20 @@
 
 # plotcsv.py
 # Plot CSV v1.0.0
-# Michael McMahon
+# Copyright (C) 2017-2020 Michael McMahon
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # Plot CSV uses python 3 because of the dependencies.  Tested with v3.5.2.
 
--- setupdeb.sh
+++ setupdeb.sh
@@ -2,7 +2,20 @@
 
 # setupdeb.sh
 # Setup DMS for Debian v1.0.0
-# Michael McMahon
+# Copyright (C) 2017-2020 Michael McMahon
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # setupdeb.sh installs the necessary packages to run builddatapack.sh
 # on Debian based GNU/Linux distributions.
