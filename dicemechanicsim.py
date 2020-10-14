71828d093f9896f8d5b4c99bb66e1b03dcf54a2b
Michael McMahon <michael@gnu.org>
2020-10-14 13:27:51 +0000

    Minor formatting

--- dicemechanicsim.py
+++ dicemechanicsim.py
@@ -3,18 +3,19 @@
 
 # dicemechanicsim.py
 # Dice Mechanic Simulation v0.98.0
-# Copyright (C) 2017-2020 Michael McMahon
 
+# Copyright (C) 2017-2020 Michael McMahon
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -37,6 +38,7 @@
 # Change this line 'file = open(filename, "w", newline="")  # Python 3'
 # to this 'file = open(filename, "wb")  # Python 2'
 # Remove "plotdicemechanic.plotaspng(filename)"
+
 
 # Import libraries
 import csv  # Export to csv format
