5fa6eaa68ad30849743f9d244ceef849cdb24cdf
Michael McMahon <michael@gnu.org>
2020-10-14 13:28:21 +0000

    Minor formatting

--- plotdicemechanic.py
+++ plotdicemechanic.py
@@ -3,18 +3,19 @@
 
 # plotdicemechanic.py
 # Plot Dice Mechanic v1.1.0
-# Copyright (C) 2017-2020 Michael McMahon
 
+# Copyright (C) 2017-2020 Michael McMahon
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -38,6 +39,7 @@
 # https://matplotlib.org/devdocs/api/_as_gen/matplotlib.pyplot.savefig.html
 # https://matplotlib.org/examples/pylab_examples/plotfile_demo.html
 # From ars at https://stackoverflow.com/questions/1186789
+
 
 # Variables
 EXAMPLECSV = "data/20180219214011.csv"
